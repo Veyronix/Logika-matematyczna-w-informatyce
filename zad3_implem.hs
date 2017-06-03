67cbc4b1d2079163580b069acc5c4ab328c74c13
Veyronix <bartekpi11@gmail.com>
2017-06-03 19:30:16 +0200

    Create zad3_implem.hs

---
+++ zad3_implem.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+type A = Int
+type B = Bool
+type C = Float
+type D = Double 
+
+
+-- impl_komp (A -> B) -> (B -> C) -> A -> C --
+impl_komp = \(x::A->B) -> \(y::B->C) -> \(z::A) -> y (x z) :: C
+
+-- impl_rozdz (A -> B) -> (A -> C) -> A -> B -> C
+impl_rozdz = \(x::A->B) -> \(y::A->C) -> \(w::A) -> \(z::B) -> y w :: C
+
+-- impl_perm (A -> B -> C) -> B -> A -> C
+impl_perm = \(x::A->B->C) -> \(y::B) -> \(z::A) -> (x z) y :: C
+
+-- impl_conj : A -> B -> A /\ B
+impl_conj = \(x::A) -> \(y::B) -> (x,y) :: (A,B)
+
+-- conj_elim_l : A /\ B -> A. 
+conj_elim_l = \(x::(A,B)) -> fst x :: A
+
+-- disj_intro_l : A -> A \/ B.
+
+
+--rozl_elim : A \/ B -> (A -> C) -> (B -> C) -> C.
+
+-- diamencik : (A -> B) -> (A -> C) -> (B -> C -> D) -> A -> D.
+diamencik = \(x::A->B) -> \(y::A->C) -> \(w::B->C->D) -> \(z::A) -> (w (x z)) (y z) :: D
+
+-- slaby_peirce : ((((A -> B) -> A) -> A) -> B) -> B.
+
+--slaby_peirce = \(x::(((A->B) -> A) -> A) -> B) -> 
+
+-- rozl_impl_rozdz : (A \/ B -> C) -> (A -> C) /\ (B -> C)
+
+-- rozl_impl_rozdz_odw : (A -> C) /\ (B -> C) -> A \/ B -> C
+
+-- curry : (A /\ B -> C) -> A -> B -> C
+curry = \(x::(A,B)->C) -> \(y::A) -> \(z::B) -> x (y,z) :: C 
+
+-- uncurry : (A -> B -> C) -> A /\ B -> C
+
+uncurry = \(x::A->B->C) -> \(y::(A,B)) -> (x (fst y)) (snd y) :: C
